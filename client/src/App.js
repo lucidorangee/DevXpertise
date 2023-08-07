dd6d3d7c04e3b8d38b85c18ec3251173e974ff89
Jihoo Ahn <lucidorangee@gmail.com>
2023-08-07 04:30:43 -0400

    Client Created

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grommet, Box, Heading } from 'grommet';
+
+function App() {
+  return (
+    <Grommet>
+      <Box pad="medium" align="center">
+        <Heading level="1">Welcome to My Grommet App!</Heading>
+      </Box>
+    </Grommet>
+  );
+}
+
+export default App;
