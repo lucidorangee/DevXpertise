34008fd35fb77067cd230f98a79d4b6faacf2740
Jihoo Ahn <lucidorangee@gmail.com>
2023-08-09 18:25:52 -0400

    Update MainPage.js

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Grommet, Box, Heading } from 'grommet';
+import { Grommet, Box, Heading, Tabs, Tab } from 'grommet';
 
 function MainPage() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const message = queryParams.get('message') === 'loggedin' ? 'Welcome back' : 'Not logged in';
     const displayName = queryParams.get('displayName') || '';
+
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabChange = index => {
+        setActiveTab(index);
+    };
     
     return (
         <Grommet>
         <Box pad="medium" align="center">
             <Heading level="1">Welcome to the Main Page!</Heading>
-            <div>
-            <h1>{message}, {displayName}</h1>
-            <Link to="/login">Go to Login Page</Link>
-            <Link to="/forum">Go to Forum Page</Link>
-            </div>
+            <Tabs activeIndex={activeTab} onActive={handleTabChange}>
+                <Tab title="Tab 1">
+                    <Box pad="medium">
+                        <Link to="/login">Go to Login Page</Link>
+                    </Box>
+                    
+                </Tab>
+                <Tab title="Tab 2">
+                    <Link to="/forum" pad="medium">Go to Forum Page</Link>
+                </Tab>
+            </Tabs>
         </Box>
         </Grommet>
       );
